7cc4913cf96fd57eb4408d7b259ec72f4cae3aa5
Madhurima Nath <31106009+mnathvt@users.noreply.github.com>
2019-03-04 14:46:58 -0500

    updated file, added comments

--- imdb_doc_list.py
+++ imdb_doc_list.py
@@ -6,6 +6,9 @@ Created on Mon Jan 28 14:04:58 2019
 @author: madhurima
 """
 
+## this file produces the plot for the number of documentaries on IMDB as a function of year
+
+#import the libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,21 +22,23 @@ def isEnglish(s):
     else:
         return True
     
-#reads the     
+#reads the .tsv file downloaded from IMDB
 f = pd.read_table('title.basics.tsv')
 
-f[['tconst', 'genres']].groupby('genres')
+#f[['tconst', 'genres']].groupby('genres')
 
+# lists the type of genres in the IMDB dataset
+ftype = f['genres']
 
-ftype = f['genres'][0]
+# extract a dataframe which says genre as documentary
+df = f.loc[ftype.str.contains('Documentary', na=False)]
 
-df = f.loc[f['genres'] == ftype]
+#retain only three columns - IMDB ID, title, startyear
 df = df[['tconst', 'primaryTitle', 'startYear']]
-
 df = df.reset_index(drop = True)
-#this resultant dataframe contains only the 3 columns - title, year and imdb ID
 
 
+## check if the titles have english letters, drop those which do not have english letters in their titles
 idx = []
 
 for i in range(len(df)):
@@ -41,31 +46,35 @@ for i in range(len(df)):
         idx.append(i)
         
 df = df.iloc[idx, :].reset_index(drop = True)
-    
+
+# to check if there are null values in the start year, if so, drop those
 print(len(df[pd.to_numeric(df['startYear'], errors='coerce').isnull()]))
 df['startYear'] = pd.to_numeric(df['startYear'], errors='coerce')
-print(len(df))
+#print(len(df))
 
 if len(df[df['startYear'].isnull()].index.tolist()) != 0:
     df.dropna(inplace=True)
-print(df[df['startYear'].isnull()].index.tolist())
-print(len(df))
+#print(df[df['startYear'].isnull()].index.tolist())
+#print(len(df))
 
-print(len(df[df['startYear'] < 1950]))
+#print(len(df[df['startYear'] < 1970]))
 
-df = df[df['startYear'] >= 1950]
+
+## retain movie titles from 1970-2018 only
+df = df[(df['startYear'] >= 1970) & (df['startYear'] < 2019)]
 df = df.reset_index(drop = True)
-print(len(df))
-df.head()
+#print(len(df))
+#df.head()
 
 df['startYear'] = df['startYear'].astype(int)
 
-df.to_csv('imdb_doc_list.csv', index = False, header = True)
+## group by the year
+#df1 = df[df['startYear'] >= 2000]
+dfyr = df[['tconst', 'startYear']].groupby('startYear')
+#dfyr = df1[['tconst', 'startYear']].groupby('startYear')
 
-df1 = df[df['startYear'] >= 2000]
-#dfyr = df[['tconst', 'startYear']].groupby('startYear')
-dfyr = df1[['tconst', 'startYear']].groupby('startYear')
 
+## finally plot the number of titles as a function of time
 plt.figure(figsize=(8,8))
 plt.plot(dfyr.startYear.first(), dfyr.tconst.nunique(), '.-',
          linewidth = 2.5, markersize = 10)
@@ -78,7 +87,6 @@ plt.ylabel('# of films', **axis_font)
 plt.xticks(**axis_tick_font)
 plt.yticks(**axis_tick_font)
 plt.xticks(np.arange(2000, 2021, 2))
-#plt.savefig('film_freq.pdf')
-plt.savefig('film_freq1.pdf')
+plt.savefig('film_freq.pdf')
 
 
